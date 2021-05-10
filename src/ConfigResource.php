70abe087eb01a3a2b99c9bc54c11d54a8fa8e693
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:11:45 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ConfigResource.php
+++ src/ConfigResource.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools-configuration for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools-configuration/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools-configuration/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\ApiTools\Configuration;
 
 use Laminas\Config\Writer\WriterInterface as ConfigWriter;
